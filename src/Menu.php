c58409b0e9e6592d91114d194b3af92efb9a476c
Egi dea ilhami <egideailhami@gmail.com>
2018-08-03 13:17:01 +0700

    add function sidebar

--- src/Menu.php
+++ src/Menu.php
@@ -30,20 +30,55 @@ class Menu
     public static function header($app)
     {
         $html ='<ul class="nav navbar-nav">';
-        foreach (DataMenu::where('id_parent',0)->get() as $key => $menu) {
+        $classMenuActive='';
+        $classSubMenuActive='';
+        foreach (DataMenu::where('id_parent',0)->where('header',0)->where('divider',0)->get() as $key => $menu) {
             $sub='';
             $tagUl='';
+            $classMenuActive = (\Request::is($menu->url) ? 'active' : '');
             foreach (DataMenu::where('id_parent',$menu->id_mnu)->get() as $key => $submenu) {
-                $sub .= ($submenu->header == 1 ? "<li class='dropdown-header'> ".$submenu->menu_ut."</li>":'<li aria-haspopup="true" class=" ">
-                <a href="'.($menu->url != null ? $menu->url :"javascript:;").'" class="nav-link  "><i class="'.$submenu->icon.'"></i> '.$submenu->menu_ut.' </a>
+                $classSubMenuActive .= (\Request::is($submenu->url) ? 'active' : '');
+                $sub .= ($submenu->header == 1 ? "<li class='dropdown-header ".(\Request::is($submenu->url) ? 'active' : '')."'> ".$submenu->menu_ut."</li>":'<li aria-haspopup="true" class="'.(\Request::is($submenu->url) ? "active" : '').'">
+                <a href="'.($submenu->url != null ? '/'.$submenu->url :"javascript:;").'" class="nav-link  "><i class="'.$submenu->icon.'"></i> '.$submenu->menu_ut.' </a>
             </li>').($submenu->divider == 1 ? "<li class='divider'> </li>":"");
                         
             $tagUl='<ul class="dropdown-menu pull-left">
             '.$sub.'</ul>';
             }
 
-            $html .='<li aria-haspopup="true" class="menu-dropdown classic-menu-dropdown">
-            <a href="'.($menu->url != null ? $menu->url :"javascript:;").'"><i class="'.$menu->icon.'"></i> '.$menu->menu_ut.'<span class="arrow"></span>
+            $html .='<li aria-haspopup="true" class="menu-dropdown classic-menu-dropdown '.$classMenuActive.$classSubMenuActive.'" >
+            <a href="'.($menu->url != null ? '/'.$menu->url :"javascript:;").'"><i class="'.$menu->icon.'"></i> '.$menu->menu_ut.'<span class="arrow"></span>
+            </a>
+           '.$tagUl.'
+        </li>';
+        }
+        $html .= '</ul>';
+        return $html;
+    }
+
+    public static function sidebar($app)
+    {
+        $html ='<ul class="page-sidebar-menu  page-header-fixed page-sidebar-menu-hover-submenu">';
+        $classMenuActive='';
+        $classSubMenuActive='';
+        foreach (DataMenu::where('id_parent',0)->where('header',0)->where('divider',0)->get() as $key => $menu) {
+            $sub='';
+            $tagUl='';
+            $classMenuActive = (\Request::is($menu->url) ? 'active' : '');
+            foreach (DataMenu::where('id_parent',$menu->id_mnu)->get() as $key => $submenu) {
+                $classSubMenuActive .= (\Request::is($submenu->url) ? 'active' : '');
+                $sub .= ($submenu->header == 1 ? "<li class='dropdown-header ".(\Request::is($submenu->url) ? 'active' : '')."'> ".$submenu->menu_ut."</li>":'<li aria-haspopup="true" class="'.(\Request::is($submenu->url) ? "active" : '').'">
+                <a href="'.($submenu->url != null ? '/'.$submenu->url :"javascript:;").'" class="nav-link  "><i class="'.$submenu->icon.'"></i> <span class="title">'.$submenu->menu_ut.'</span></a>
+            </li>').($submenu->divider == 1 ? "<li class='divider'> </li>":"");
+                        
+            $tagUl='<ul class="sub-menu">
+            '.$sub.'</ul>';
+            }
+
+            $html .='<li class="nav-item start '.$classMenuActive.$classSubMenuActive.'" >
+            <a href="'.($menu->url != null ? '/'.$menu->url :"javascript:;").'"><i class="'.$menu->icon.'"></i>
+            <span class="title"> '.$menu->menu_ut.'</span>
+            <span class="arrow"></span>
             </a>
            '.$tagUl.'
         </li>';
