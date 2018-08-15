1b47231121de0c991e0a1af18a4fce37145352a7
Egi dea ilhami <egideailhami@gmail.com>
2018-08-15 11:13:23 +0700

    add select 2

--- src/Menu.php
+++ src/Menu.php
@@ -116,6 +116,7 @@ class Menu
         $html = "
 <script type=\"text/javascript\" src=\"".asset('vendor/fontawesome-iconpicker/js/fontawesome-iconpicker.js')."\"></script>
 <script type=\"text/javascript\" src=\"".asset('vendor/js/main.js')."\"></script>
+<script type=\"text/javascript\" src=\"".asset('vendor/select2/select2.full.min.js')."\"></script>
         <script>
         
     $(document).ready(function() {
