af13655ef4b8cec18176754f28f060879b8305c0
Egi dea ilhami <egideailhami@gmail.com>
2019-01-14 12:04:26 +0700

    edit service provider

--- src/MenuServiceProvider.php
+++ src/MenuServiceProvider.php
@@ -14,17 +14,19 @@ class MenuServiceProvider extends ServiceProvider
 
         $this->loadViewsFrom(__DIR__ . '/views', 'grittekno.menu');
 
-        $this->publishes([
-            __DIR__ . '/migrations' => database_path('migrations'),
-        ], 'grittekno-migrations');
+        $this->commands('command.grittekno.migration');
 
-        $this->publishes([
-            __DIR__ . '/seeds' => database_path('seeds'),
-        ], 'grittekno-seeds');
+        // $this->publishes([
+        //     __DIR__ . '/migrations' => database_path('migrations'),
+        // ], 'grittekno-migrations');
 
-        $this->publishes([
-            __DIR__ . '/assets' => public_path('vendor'),
-        ], 'grittekno-assets');
+        // $this->publishes([
+        //     __DIR__ . '/seeds' => database_path('seeds'),
+        // ], 'grittekno-seeds');
+
+        // $this->publishes([
+        //     __DIR__ . '/assets' => public_path('vendor'),
+        // ], 'grittekno-assets');
         
         // $this->publishes([
         //     __DIR__ . '/views' => base_path('resources/views/'.env('menu_path')),
@@ -33,7 +35,7 @@ class MenuServiceProvider extends ServiceProvider
 
     public function register()
     {
-        $this->app->bind('egideailhami.menu', function () {
+        $this->app->bind('grittekno.menu', function () {
             return new Menu();
         });
     }
